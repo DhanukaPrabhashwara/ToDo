db47380429badf6004990b40e332bc31ddf21d89
dhanuka prabhashwara <dhanukaprabha@gmail.com>
2024-05-19 00:39:39 +0530

    bug fixed

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.todo
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,10 +16,17 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val welcome : Button = findViewById(R.id.button)
-        welcome.setOnClickListener{
-            val intent = Intent(this, toDoList::class.java)
+//        val welcome : Button = findViewById(R.id.button)
+//        welcome.setOnClickListener{
+//            val intent = Intent(this, toDoList::class.java)
+//            startActivity(intent)
+//        }
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, toDoList::class.java)
             startActivity(intent)
-        }
+            finish()
+        },2000)
     }
 }
